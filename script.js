e675869f941b32bc7a89da4143b31f1f66f45cc8
Peter <pwjablonski@gmail.com>
2017-07-27 11:21:13 -0400

    changes

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
+// Class 1:
+  // 1.1 
+    // Fork and clone starter repo and set up workspace.
+  // 1.2
+    // Create HTML intial state - Explore Giphy API Documentation
 
-$(".btn").click(function(){
-    $('.gallery').empty();
-    var searchTerm = $('#srch-term').val();
-    search(searchTerm);
-});
+// Class 2:
+  // 2.1 Create a keypress function that 
+    // takes in the value from the the search box when the enter button is pressed
+    // empty's the elements on the screen
+    // Displays the search term to the console
+  // 2.2 
+    // Define a function called search 
+    // takes a string searchTerm as a parameter
+    // makes a request to the giphy API using the searchTerm
+    // logs the the reponse to the console in the success function
+    // Call the search function in the keypress function passing the search term as a parameter  
+
+// Class 3:
+  // 3.1
+    // Create the display results function
+    // Displays a single image thumbnails on the screen
+    // Call the Display Results function in the success function passing the reponse as a parameter
+  // 3.2 
+    // Update the fuction to display all the image thumbnails to the screen
+  
+    
+
+// *************************** YOUR CODE BELOW *******************************
+//******************TEST EARLY AND OFTEN USING console.log() ******************
+//****************** SERIOUSLY TEST USING console.log()!!! ******************
+
+// .keypress function
 
 
-$("body").keypress(function(e){
-    console.log(e.which)
-    if(e.which === 13 ) {
-	    $('.gallery').empty();
-	    var searchTerm = $('#srch-term').val();
-	    search(searchTerm);
-	}
-});
 
 
-function displayResults(response){
-	 for(var i = 0; i < response.data.length; i++){
-           console.log(response.data[i].images.fixed_width.url);
-            $('.gallery').append(
-                '<div class="col-lg-3 col-md-4 col-xs-6 thumb">\
-                    <a class="thumbnail">\
-                        <img src=' + response.data[i].images.fixed_width.url + '/>\
-                    </a>\
-                </div>'
-            );
-      }
-}
+// search function
 
 
-function search(searchTerm){
-    var url = "https://api.giphy.com/v1/stickers/search?q=" + searchTerm + "&api_key=dc6zaTOxFJmzC";
-    console.log(url);
-    $.ajax({
-        url: url,
-        method: "GET",
-        success: function(response){
-          	displayResults(response);
-        },
-    }); 
-}
+
+
+// displayResults function
