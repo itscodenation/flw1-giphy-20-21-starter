28b9a0c78f94b1e952f9de221312869c47b7e9ec
Peter Jablonski <pwjablonski@gmail.com>
2018-02-09 15:58:14 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,37 +1,10 @@
-// Class 1:
-  // 1.1 
-    // Fork and clone starter repo and set up workspace.
-  // 1.2
-    // Create HTML intial state - Explore Giphy API Documentation
-
-// Class 2:
-  // 2.1 Create a keypress function that 
-    // takes in the value from the the search box when the enter button is pressed
-    // empty's the elements on the screen
-    // Displays the search term to the console
-  // 2.2 
-    // Define a function called search 
-    // takes a string searchTerm as a parameter
-    // makes a request to the giphy API using the searchTerm
-    // logs the the reponse to the console in the success function
-    // Call the search function in the keypress function passing the search term as a parameter  
-
-// Class 3:
-  // 3.1
-    // Create the display results function
-    // Displays a single image thumbnail on the screen
-    // Call the Display Results function in the success function passing the reponse as a parameter
-  // 3.2 
-    // Use Math.random to randomly select an image from the data array 
-
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
-// .keypress function
-
-
-// search function
-
-
-// displayResults function
+$(document).ready(function(){
+  
+  
+  
+  
+});
