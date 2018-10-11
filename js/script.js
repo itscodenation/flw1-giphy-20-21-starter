a232a99621eec7ca0eee502c61d1048c9b754ab6
MichaelOppong <michael@scripted.org>
2018-10-11 15:50:31 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
+/* global $ */
 
 $("#search-button").click(function(){
   
