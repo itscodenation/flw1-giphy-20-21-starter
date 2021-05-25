6ddd73d39d732d31d8ad2e26ccbe5d4be59d429b
Glitch (t-giphy) <none>
2021-05-25 23:33:16 +0000

    🐗🚈 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -3,9 +3,29 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
 
+let custom_api_url = "https://api.giphy.com/v1/gifs/search?rating=pg&api_key=dc6zaTOxFJmzC&q=";
+
+
 $(".search-button").click(function(){
   
+  let search = custom_api_url + $(".search-term").val();
+  
+  fetch(search).then(function(response) {
+    return response.json();
+  }).then(function(data) {
+    
+    let index = Math.floor(Math.random() * data.data.length);
+    let gif = `<img src="${data.data[index].images.original.url}">`;
+    
+    $(".main").append(gif);
+    
+    $(".mail").attr(`href`, `mailto:s_tomas.brubaker@ousd.org?subject=Important!&body=` + gif);
+    
+  });
 
   
 });
 
+$(".clear").click(function(){
+  $(".main").html("");
+});
