cdedc0d28dea1b31a009c74846f9fe7ff8d30f08
Glitch (api-carlos) <none>
2022-02-28 03:28:17 +0000

    🏵️🐙 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -3,9 +3,24 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
 
-$(".search-button").click(function(){
-  
+$(".search-button").click(function() {
+  let shortTerm = $(".search-term").val();
 
-  
+  let API_URL =
+    "https://api.giphy.com/v1/gifs/search?q=" +
+    shortTerm +
+    "&rating=pg&api_key=dc6zaTOxFJmzC";
+  fetch(API_URL)
+    .then(function(response) {
+      console.log(response);
+      return response.json();
+    })
+
+    .then(function(gifdata) {
+      console.log(gifdata);
+      var x = Math.floor((Math.random() * gifdata.data.length) + 1 );
+      var picURL = gifdata.data[x].images.original.url;
+      console.log(picURL);
+      $(".main").html("<img src=" + picURL + ">");
+    });
 });
-
