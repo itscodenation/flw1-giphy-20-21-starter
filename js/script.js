0b8e4d825d14df449e5e11fea60b2e3b10b7a76a
Glitch (api-giphy1234566) <none>
2022-04-21 00:55:19 +0000

    🪘▪️ Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 $(".search-button").click(function(){
-    fetch("https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=tTVMCPwEb1NapUWHla1pBNt4jKlfEqo1")
+  let searchTerm = $(".search-term").val()
+  $(".search-term").val("")
+  let url = `https://api.giphy.com/v1/gifs/search?q=${searchTerm}&rating=pg&api_key=tTVMCPwEb1NapUWHla1pBNt4jKlfEqo1`
+    fetch(url)
       .then(function(response){
         return response.json();
     })
       .then(function(data){
-        console.log(data.data)
+        let www = data.data.length
+        console.log(data.data);
+        let abcd = Math.floor(Math.random()*www);
+        console.log(`list#: ${abcd}`);
+        let number = data.data[abcd].images.original.url
+        console.log(number);
+        $(".main").html(`<img height="250px" src="${number}">`);
       })
 });
